24994f5884b014449b51e12eab7b30c6bce9930b
mmedoo <mmedoo.active@gmail.com>
2024-09-26 10:58:04 +0300

    Refactor server.js to remove logger middleware and unused code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const logger = require('morgan');
+// const logger = require('morgan');
 const routers = require('./routes/all_routers');
 const path = require("path");
 require('dotenv').config();
@@ -9,9 +9,9 @@ app.set('view-engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // app.use(logger('dev'));
-app.use(express.json());
+// app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 
 for (let route of Object.values(routers)) {
